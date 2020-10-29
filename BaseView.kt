36f5cb6a438f21834a6fc9fdbf77b3e6e8f5fde7
Ahmad Nashihuddien <ahmad.nshdn@gmail.com>
2020-10-29 14:49:24 +0700

    Update BaseView.kt

--- BaseView.kt
+++ BaseView.kt
@@ -1,1 +1,10 @@
 // please create basic interface for activity when connecting and received response from backend server
+
+interface BaseView {
+  fun onSuccessGetData()
+  fun showGeneralError()
+  fun showInetError()
+  fun onLoadingButton()
+  fun onFailGetData()
+  fun onFailValidate()
+}
